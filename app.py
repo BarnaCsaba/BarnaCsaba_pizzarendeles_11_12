f6a4ef7cf8b505a361207eb6ceba5585c8c96f58
Csib_Csab <barna.csaba@diak.szbi-pg.hu>
2024-11-12 10:46:04 +0100

    vgsy

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
+orders = []
 
 @app.route('/', methods=['GET'])
 def index():
+    
+        
     return render_template("index.html")
 
-@app.route('/pizza-order', methods=['GET'])
+@app.route('/pizza-order', methods=['GET', 'POST'])
 def pizza_order():
+    if request.method == 'POST':
+        _name = request.form.get['name']
+        _phone = request.form.get['phone']
+        _size = request.form.get['size']
+        orders = {"name": _name,
+                       "phone": _phone,
+                       "size": _size}
+        orders.append(order)
+        print(orders)
+        #return redirect(url_for("index") orders=orders)
     return render_template("pizza_order.html")
 
 if __name__ == "__main__":
